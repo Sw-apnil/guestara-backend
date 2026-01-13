70297427ffebe99d3fde94ca306540f7d7853a14
Sw-apnil <swapnil414416@gmail.com>
2026-01-14 02:25:05 +0530

    basic structure done

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const app = require('./app')
 const { PORT } = require('./config/env')
+const connectDB = require('./config/db')
+
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`)
